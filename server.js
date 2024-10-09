8ec349414be063eab60b390cca8203fcd608a93d
TulioGamer503 <105886705+TulioGamer503@users.noreply.github.com>
2024-10-09 13:17:36 -0600

    as

--- server.js
+++ server.js
@@ -18,14 +18,32 @@ app.post('/api/sensores', (req, res) => {
     io.emit('actualizar-sensores', datosSensor);
     res.status(200).send({ status: 'success', data: datosSensor });
 });
-app.post('/api/sensores', (req, res) => {
-    const datosSensor = req.body;
-    console.log("Datos recibidos del proxy local:", datosSensor);
 
-    // Enviar los datos a todos los clientes conectados
-    io.emit('actualizar-sensores', datosSensor);
-    res.status(200).send({ status: 'success', data: datosSensor });
+// Rutas para encender y apagar LEDs
+app.post('/api/encender', (req, res) => {
+    const sensorId = req.body.sensorId;
+    if (sensorId) {
+        // Emitir el evento de encender LED
+        io.emit('encender-led', { sensorId });
+        console.log(`LED ${sensorId} encendido.`);
+        res.send("LED encendido.");
+    } else {
+        res.status(400).send("ID del sensor no proporcionado.");
+    }
 });
+
+app.post('/api/apagar', (req, res) => {
+    const sensorId = req.body.sensorId;
+    if (sensorId) {
+        // Emitir el evento de apagar LED
+        io.emit('apagar-led', { sensorId });
+        console.log(`LED ${sensorId} apagado.`);
+        res.send("LED apagado.");
+    } else {
+        res.status(400).send("ID del sensor no proporcionado.");
+    }
+});
+
 // Servir archivos estáticos
 app.use(express.static('public'));
 
